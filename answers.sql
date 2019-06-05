40df72455dd2da1566fedb11d92b313643c60939
david <davidwnewman91@gmail.com>
2019-06-05 14:03:53 -0500

    initial commit

---
+++ answers.sql
@@ -0,0 +1,8 @@
+-- SELECT [Column names] 
+-- FROM [table] [abbv]
+-- JOIN [table2] [abbv2] ON abbv.prop = abbv2.prop WHERE [Conditions];
+
+-- SELECT a.name, b.name FROM some_table a JOIN another_table b ON a.some_id = b.some_id;
+-- SELECT a.name, b.name FROM some_table a JOIN another_table b ON a.some_id = b.some_id WHERE b.email = 'e@mail.com';
+
+SELECT 
