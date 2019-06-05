c57393b2e11e0e25547da82e3b55c54c646ddfaa
david <davidwnewman91@gmail.com>
2019-06-05 15:50:16 -0500

    Update row

--- answers.sql
+++ answers.sql
@@ -117,17 +117,26 @@ WHERE album_id IN (
 
 -- UPDATE athletes SET sport = 'Picklball' WHERE sport = 'pockleball';
 
-1) Find all customers with fax numbers and set those numbers to null.
+1) UPDATE customer SET fax = NULL
+WHERE fax IS NOT NULL
 
-2) Find all customers with no company (null) and set their company to "Self".
+2) UPDATE customer SET company = 'Self'
+WHERE company IS NULL;
 
-3) Find the customer Julia Barnett and change her last name to Thompson.
+3) UPDATE customer SET last_name = 'Thompson'
+WHERE first_name = 'Julia' AND last_name = 'Barnett';
 
-4) Find the customer with this email luisrojas@yahoo.cl and change his support rep to 4.
+4) UPDATE customer SET support_rep_id = 4
+WHERE email = 'luisrojas@yahoo.cl';
 
-5) Find all tracks that are the genre Metal and have no composer. Set the composer to "The darkness around us".
+5) UPDATE track SET composer = 'The darkness around us'
+WHERE genre_id = (
+  SELECT genre_id FROM genre
+  WHERE name = 'Metal'
+  ) AND composer IS NULL;
 
 6) Refresh your page to remove all database changes.
 
 
 
+-- GROUP BY --
